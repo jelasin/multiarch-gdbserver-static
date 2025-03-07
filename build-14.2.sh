0067f869e771ab16c8b7546f0a2e127b6c5696e4
Jelasin <97788570+jelasin@users.noreply.github.com>
2025-03-07 11:46:16 +0800

    Update build-14.2.sh

--- build-14.2.sh
+++ build-14.2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-CC="riscv64-linux-gnu-gcc"
-CXX="riscv64-linux-gnu-g++"
+export PATH="/home/pwn/aarch/aarch64/aarch64-linux-musl-cross/bin:$PATH"
+export CC="aarch64-linux-musl-gcc"
+export CXX="aarch64-linux-musl-g++"
 
-MY_HOST="riscv64-linux-gnu"
-MY_TARGET="riscv64-linux-gnu"
+MY_HOST="aarch64-linux-gnu"
+MY_TARGET="aarch64-linux-gnu"
 
 build_dir=$(pwd)
 gnulib_dir=$build_dir/../gnulib
@@ -18,26 +19,27 @@ if [ ! -d "$gnulib_dir" ] || [ ! -d "$gdbsupport_dir" ] || [ ! -d "$libiberty_di
 fi
 
 cd $gnulib_dir
-./configure --host=$MY_HOST --target=$MY_TARGET \
+./configure --host=$MY_HOST --target=$TARGET_CROSS \
             CFLAGS="-static" CXXFLAGS="-static" LDFLAGS="-static"
 make -j$(nproc)
 
 cd $gdbsupport_dir
-./configure --host=$MY_HOST --target=$MY_TARGET \
+./configure --host=$MY_HOST --target=$TARGET_CROSS \
             CFLAGS="-static" CXXFLAGS="-static" LDFLAGS="-static"
 make -j$(nproc)
 
 cd $libiberty_dir
-./configure --host=$MY_HOST --target=$MY_TARGET \
+./configure --host=$MY_HOST --target=$TARGET_CROSS \
             CFLAGS="-static" CXXFLAGS="-static" LDFLAGS="-static"
 make -j$(nproc)
 
 cd $build_dir
 
-$gdbserver_dir/configure --host=$MY_HOST --target=$MY_TARGET \
+$gdbserver_dir/configure --host=$MY_HOST --target=$TARGET_CROSS \
             CFLAGS="-static" CXXFLAGS="-static" \
-            LDFLAGS="-static -L$gnulib_dir -L$gdbsupport_dir -L$libiberty_dir"
+            LDFLAGS="-static -L$library_path -L$gnulib_dir -L$gdbsupport_dir"
 
 make -j$(nproc)
 
 echo "Compilation finished!"
+
