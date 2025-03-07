7e4c01c1aec451e53b07d839b869151955070248
Jelasin <97788570+jelasin@users.noreply.github.com>
2025-03-07 15:21:01 +0800

    Update build-9.2.sh

--- build-9.2.sh
+++ build-9.2.sh
@@ -1,24 +1,5 @@
 # 9.2 not support riscv
 
-sudo apt install gcc-arm-linux-gnueabi g++-arm-linux-gnueabi -y
-sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf -y
-sudo apt install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu -y
-
-sudo apt install gcc-mipsel-linux-gnu g++-mipsel-linux-gnu -y
-sudo apt install gcc-mips64el-linux-gnuabi64 g++-mips64el-linux-gnuabi64 -y
-
-sudo apt install gcc-mips-linux-gnu g++-mips-linux-gnu -y
-sudo apt install gcc-mips64-linux-gnuabi64 g++-mips64-linux-gnuabi64 -y
-
-sudo apt install gcc-powerpc-linux-gnu g++-powerpc-linux-gnu -y
-sudo apt install gcc-powerpc64-linux-gnu g++-powerpc64-linux-gnu -y
-
-sudo apt install gcc-riscv64-linux-gnu g++-riscv64-linux-gnu -y
-
-sudo apt install gcc-alpha-linux-gnu g++-alpha-linux-gnu -y
-sudo apt install gcc-s390x-linux-gnu g++-s390x-linux-gnu -y
-sudo apt install gcc-sparc64-linux-gnu g++-sparc64-linux-gnu -y
-
 curl https://ftp.gnu.org/gnu/gdb/gdb-9.2.tar.gz --output gdb-9.2.tar.gz
 tar -zxvf ./gdb-9.2.tar.gz
 cd gdb-9.2
